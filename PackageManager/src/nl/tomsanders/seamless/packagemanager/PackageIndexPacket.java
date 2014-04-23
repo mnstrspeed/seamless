7ccb249d848cb21f362344a40a8a22f7f462e283
Tom Sanders <mail@tomsanders.nl>
2014-04-23 12:53:15 +0200

    Send broadcast before listening This way we don't receive our own broadcast. DAMN IM SMART

--- PackageManager/src/nl/tomsanders/seamless/packagemanager/PackageIndexPacket.java
+++ PackageManager/src/nl/tomsanders/seamless/packagemanager/PackageIndexPacket.java
@@ -1,6 +1,5 @@
 package nl.tomsanders.seamless.packagemanager;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @SuppressWarnings("serial")
--- PackageManager/src/nl/tomsanders/seamless/packagemanager/PackageManager.java
+++ PackageManager/src/nl/tomsanders/seamless/packagemanager/PackageManager.java
@@ -70,10 +70,10 @@ public class PackageManager implements DiscoveryService.DiscoveryServiceListener
 		this.discoveryService = new DiscoveryService("Dave?", DISCOVERY_PORT);
 		Log.v("Waiting for network interface");
 		this.discoveryService.waitForNetwork();
-		Log.v("Listening for discovery broadcasts on local network");
-		this.discoveryService.startListening(this);
 		Log.v("Sending discovery broadcast on local network");
 		this.discoveryService.sendBroadcast();
+		Log.v("Listening for discovery broadcasts on local network");
+		this.discoveryService.startListening(this);
 	}
 	
 	public void onHostDiscovered(InetAddress address) 
