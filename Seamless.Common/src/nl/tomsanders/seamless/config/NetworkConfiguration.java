b1769b02e36a9f888d66e0f3f22845507e93cd45
Tom Sanders <mail@tomsanders.nl>
2014-05-01 15:19:44 +0200

    Unified ports. Fixes #12

--- Seamless.Common/src/nl/tomsanders/seamless/config/NetworkConfiguration.java
+++ Seamless.Common/src/nl/tomsanders/seamless/config/NetworkConfiguration.java
@@ -4,9 +4,9 @@ public abstract class NetworkConfiguration
 {
 	public static final String LOCAL_HOST = "127.0.0.1";
 	
-	public static final int INSTANCESERVER_DISCOVERY_PORT = 1809;
 	public static final int INSTANCESERVER_LOCAL_PORT = 1901;
-	public static final int INSTANCESERVER_REMOTE_PORT = 9501;
+	public static final int INSTANCESERVER_DISCOVERY_PORT = 1902;
+	public static final int INSTANCESERVER_REMOTE_PORT = 1903;
 	
 	public static final int PACKAGEMANAGER_DISCOVERY_PORT = 1811;
 	public static final int PACKAGEMANAGER_PORT = 1812;
